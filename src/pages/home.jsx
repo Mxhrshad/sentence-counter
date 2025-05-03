87688c6e150d1ce853b3e7a6e8353060c5973957
Mxhrshad <mserattalab@gmail.com>
2025-05-03 19:24:57 +0330

    Add Home component and Input/Output components for sentence counter

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import Input from "../components/input";
+import Output from "../components/output";
+
+export default function Home() {
+
+    return (
+        <div className="w-full h-screen bg-amber-100 flex flex-col items-center justify-center">
+            <Input />
+            <Output />
+        </div>
+    )
+
+}
