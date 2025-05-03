bb6862f42ec9e54f119bf50a1ef6f1cecbc2e8d1
Mxhrshad <mserattalab@gmail.com>
2025-05-03 19:26:23 +0330

    Implement Input component with sentence change handling in Home page

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,3 +1,27 @@
-export default function Input(){
-    
+import { useState } from "react";
+
+export default function Input({ onSentenceChange }) {
+    const [sentence, setSentence] = useState("");
+
+    const handleChange = (e) => {
+        const newSentence = e.target.value;
+        setSentence(newSentence);
+        onSentenceChange(newSentence);
+    };
+
+    return (
+        <div className="w-full h-1/2 flex justify-center items-center flex-col">
+            <h1 className="font-bold text-5xl mb-5">Sentence Counter</h1>
+            <p className="font-semibold">Type your sentence</p>
+            <input
+                className="bg-white text-gray-800 border border-amber-300 rounded-full px-6 py-3 mt-4 shadow-lg focus:outline-none focus:ring-4 focus:ring-amber-400 transition duration-300 w-80 placeholder-gray-400 text-center"
+                type="text"
+                name="sentence"
+                id="sentence"
+                placeholder="Start typing..."
+                value={sentence}
+                onChange={handleChange}
+            />
+        </div>
+    );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import Input from "../components/input";
 import Output from "../components/output";
 
 export default function Home() {
+    const [paragraph, setParagraph] = useState("");
 
     return (
         <div className="w-full h-screen bg-amber-100 flex flex-col items-center justify-center">
-            <Input />
+            <Input  onSentenceChange={setParagraph} />
             <Output />
         </div>
     )
